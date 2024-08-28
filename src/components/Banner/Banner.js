3fb16ca381ab3561b2bcccbb5de73cb1aee1cc80
Lucas Menon <lucasguilarduccimenon@gmail.com>
2024-08-28 15:58:39 -0300

    little alterations

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header className="banner">
+      <img src="/images/banner.png" alt="banner da pagina" />
+    </header>
+  );
+}
+
+export default Banner;
