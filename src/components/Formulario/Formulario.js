24c1248e7401f318e9c79ca9d654df0684b691e7
Lucas Menon <lucasguilarduccimenon@gmail.com>
2024-08-30 15:48:34 -0300

    Botao e Lista adicionados

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,14 +1,34 @@
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto/CampoTexto";
+import ListaSuspensa from "../ListaSuspensa/ListaSuspensa";
+import Botao from "../Botao/Botao";
 
 const Formulario = () => {
+  
+  const times = [
+    'Front-End',
+    'Back-End',
+    'Data-Sience',
+    'Devops',
+    'UX/UI',
+    'Mobile',
+    'Inovação e Gestão'
+  ]
+
+  const onSave = (event) => {
+    event.preventDefault()
+    console.log('Salvando dados')
+  }
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={onSave}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Url da imagem" />
+        <CampoTexto obrigatorio="true" label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio="true" label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigatorio="true" label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={times}/>
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
---
+++ src/components/ListaSuspensa/ListaSuspensa.js
@@ -0,0 +1,18 @@
+import "./ListaSuspensa.css";
+
+const ListaSuspensa = (props) => {
+  return (
+    <div className="lista-suspensa">
+      <label>
+        {props.label}
+        <select>
+          {props.itens.map((item) => (
+            <option key={item}>{item}</option>
+          ))}
+        </select>
+      </label>
+    </div>
+  );
+};
+
+export default ListaSuspensa;
